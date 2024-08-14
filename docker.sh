100df11725809f9e83c0f6178e553241ec118f17
Daniel Rossi <electroteque@protonmail.com>
2024-08-15 05:34:05 +1000

    update dockerfile add pi dockerfil

--- docker.sh
+++ docker.sh
@@ -2,6 +2,7 @@
 
 REPO="danrossi/gstreamer"
 IMAGE="gst-build-cross"
+IMAGE_PI="gst-build-cross-pi"
 VERSION=$(cat VERSION)
 WORKDIR="/build"
 
@@ -11,6 +12,9 @@ case "$1" in
     ;;
   build)
     docker build --tag ${REPO}/${IMAGE}:${VERSION} --file Dockerfile .
+    ;;
+  buildpi)
+    docker build --tag ${REPO}/${IMAGE_PI}:${VERSION} --file Dockerfile.pi .
     ;;
   *)
     echo "Usage: $0 {run|build}"
