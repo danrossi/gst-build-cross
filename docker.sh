1c81083824f4365e9e7ee16d1a42ae8d117de5aa
Daniel Rossi <electroteque@protonmail.com>
2024-08-16 05:51:39 +1000

    add ubuntu image build

--- docker.sh
+++ docker.sh
@@ -3,6 +3,7 @@
 REPO="danrossi/gstreamer"
 IMAGE="gst-build-cross"
 IMAGE_PI="gst-build-cross-pi"
+IMAGE_UBUNTU="gst-build-cross-ubuntu"
 VERSION=$(cat VERSION)
 WORKDIR="/build"
 
@@ -12,6 +13,9 @@ case "$1" in
     ;;
   build)
     docker build --tag ${REPO}/${IMAGE}:${VERSION} --file Dockerfile .
+    ;;
+  buildubuntu)
+    docker build --tag ${REPO}/${IMAGE_UBUNTU}:${VERSION} --file Dockerfile.ubuntu .
     ;;
   buildpi)
     docker build --tag ${REPO}/${IMAGE_PI}:${VERSION} --file Dockerfile.pi .
