322f9425c41444b1f74a98f9fb949ead709ade82
Daniel Rossi <electroteque@protonmail.com>
2024-08-21 04:39:41 +1000

    fix debian installer for multiarch

--- dist/debian/bookworm/install.sh
+++ dist/debian/bookworm/install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#sudo apt install libunwind-dev libdw-dev
-#sudo apt --fix-broken install
-#sudo dpkg -i *.deb
-sudo apt-get install ./gstreamer1.0_1.25.0.1_amd64.deb
+
+arch=$(dpkg-architecture -qDEB_HOST_ARCH)
+sudo apt-get install ./gstreamer1.0_1.25.0.1_${arch}.deb
